f26cb77933259f6ee164947e4c7a039678fef06e
Guillaume Larose <theonlyguills@gmail.com>
2024-10-27 20:52:50 -0400

    start with wezterm and neovim

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.default_domain = 'WSL:Ubuntu-24.04'
+
+config.color_scheme = 'gogh'
+
+config.window_frame = {
+  font = wezterm.font { family = 'Fira Code', weight = 'Bold' },
+  font_size = 12.0,
+  active_titlebar_bg = '#333333',
+  inactive_titlebar_bg = '#777777',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
+
+config.font = wezterm.font 'Fira Code'
+
+config.window_background_opacity = .85
+
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require("config.lazy")
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.cmd('hi Normal ctermbg=NONE guibg=NONE')
+vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
+
