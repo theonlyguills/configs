99cc18d672541528ab693f4db52b2176b0247810
Guillaume Larose <Guillaume.Chamberland-Larose@dfo-mpo.gc.ca>
2025-07-30 15:12:24 -0400

    clone repo on wsl side too, ignore dpkg error

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,8 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
+git clone https://github.com/theonlyguills/configs
+
 mkdir ~/.config
 cp ~/configs/nvim ~/.config -r
 
