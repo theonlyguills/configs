74feb4cf298358b27554e087bef0d9249130aaf6
Guillaume Larose <Guillaume.Chamberland-Larose@dfo-mpo.gc.ca>
2025-07-31 07:56:09 -0400

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,14 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
+echo "export PATH=\$PATH:/opt/nvim-linux-x86_64/bin" >> ~/.bashrc
+
 git clone https://github.com/theonlyguills/configs
 
 mkdir ~/.config
 cp ~/configs/nvim ~/.config -r
 
+sudo apt update
 sudo apt install ripgrep
 
 echo "Installing taskfile"
@@ -20,6 +23,9 @@ sudo cp ~/.local/bin/task /usr/local/bin/
 
 echo "alias t=task" >> ~/.bashrc
 echo "alias tl=\"task --list-all\"" >> ~/.bashrc
+echo "alias vi=nvim" >> ~/.bashrc
+echo "alias vim=nvim" >> ~/.bashrc
+
 source ~/.bashrc
 
 cd ~/configs
